3c1f50ec928e34b35fb6adc7d478a25425b655a1
rutujak24 <rutujakawade24@gmail.com>
2025-10-14 21:41:46 -0400

    Fix run-sample.sh to use project-relative path

--- hack/run-sample.sh
+++ hack/run-sample.sh
@@ -5,6 +5,7 @@ OUT_FILE="sample-recommendation.yaml"
 
 echo "Running analyzer sample and writing recommendation to ${OUT_FILE}"
 
-go run ../cmd/controller --sample > "${OUT_FILE}"
+# Run from project root: use the cmd/controller path relative to repo root
+go run ./cmd/controller --sample > "${OUT_FILE}"
 
 echo "Wrote ${OUT_FILE}"
